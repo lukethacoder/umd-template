c447c0bb48a258f1ada5f5d85a5d82afb3baa916
Luke Secomb <luke.secomb@live.com>
2021-06-10 20:26:05 +1000

    🔐 add dotenv + pull package name / update README.md

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 rollup based UMD package template
 
+Package name should be set in the `.env` file under `PACKAGE_NAME=`
+
 > based on [this](https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/) blog post
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('Creating package with name ', process.env.PACKAGE_NAME);
 
 const config = {
   input: 'index.js',
   output: {
-    file: 'dist/bundle.js',
+    file: 'bundle.js',
     format: 'umd',
-    name: 'MyModuleName',
+    name: process.env.PACKAGE_NAME,
   },
   plugins: [commonjs(), resolve()],
 };
