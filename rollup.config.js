0e9d91fbb584676fad92c84d3dbeca8f0bc0e003
Luke Secomb <luke.secomb@live.com>
2021-06-11 10:26:28 +1000

    ⚙ add `@rollup/plugin-json` to parse json source / add env to adjust `PACKAGE_NAME` and `NODE_ENV`

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 console.log('Creating package with name ', process.env.PACKAGE_NAME);
+
+const plugins = [json(), commonjs(), resolve()];
+
+// if marked as production in .env include the minify + obfuscation
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(terser());
+}
 
 const config = {
   input: 'index.js',
@@ -12,7 +22,7 @@ const config = {
     format: 'umd',
     name: process.env.PACKAGE_NAME,
   },
-  plugins: [commonjs(), resolve()],
+  plugins,
 };
 
 export default config;
